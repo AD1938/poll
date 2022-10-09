afbc4e5744a55dfba9773125407aa1ef363527d3
Ando Deng <87212291+AD1938@users.noreply.github.com>
2022-10-09 18:50:42 -0400

    part 3 done

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from curses.ascii import HT
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
 from django.template import loader
+
 # Create your views here.
 
 
@@ -16,7 +18,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
